c5c48b2bb78d4d28fc13b61c02bd07565989f7d6
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-09-15 15:28:43 +0100

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import listEndpoints from 'express-list-endpoints'
-import authorsRouter from './services/authors/index.js'
+import blogPostsRouter from './services/blogPosts/index.js'
+import cors from "cors"
 
 const server = express()
 
 const port = 3001    
 
+server.use(cors()) 
 server.use(express.json())
 
-server.use("/authors", authorsRouter)
+server.use("/blogPosts", blogPostsRouter)
 console.table(listEndpoints(server))
 
 server.listen(port, () => {
---
+++ src/services/blogPosts/index.js
@@ -0,0 +1,85 @@
+
+
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import uniqid from 'uniqid'
+
+const blogPostsRouter = express.Router()
+
+const currentFilePath = fileURLToPath(import.meta.url)
+
+const currentDirPath = dirname(currentFilePath)
+
+const blogPostsJSONFilePath = join(currentDirPath, "blogPosts.json")
+
+
+
+blogPostsRouter.post("/", (req, res) => {
+console.log(req.body)
+
+const newblogPost = {...req.body, id:uniqid(), createdAt: new Date()}
+
+const blogPosts = JSON.parse(fs.readFileSync(blogPostsJSONFilePath))
+
+blogPosts.push(newblogPost)
+
+fs.writeFileSync(blogPostsJSONFilePath, JSON.stringify(blogPosts))
+
+    res.status(201).send({id: newblogPost.id})
+})
+
+
+
+blogPostsRouter.get("/", (req, res) => {
+    const fileContent = fs.readFileSync(blogPostsJSONFilePath)
+    console.log(JSON.parse(fileContent))
+
+    const blogPosts = (JSON.parse(fileContent))
+
+    res.send(blogPosts)  
+})
+
+
+
+blogPostsRouter.get("/:blogPostID", (req, res) => {
+
+    const blogPosts = JSON.parse(fs.readFileSync(blogPostsJSONFilePath))
+
+    const blogPost = blogPosts.find(a => a.id === req.params.blogPostID)
+
+    res.send(blogPost)
+})
+
+
+
+blogPostsRouter.put("/:blogPostID", (req, res) =>{
+    const blogPosts = JSON.parse(fs.readFileSync(blogPostsJSONFilePath))
+
+    const remainingblogPosts = blogPosts.filter(blogPosts => a.id !== req.params.blogPostID)
+
+    const updatedblogPosts = {...req.body, id:req.params.blogPostID}
+
+    remainingblogPosts.push(updatedblogPosts)
+
+    fs.writeFileSync(blogPostsJSONFilePath, JSON.stringify(remainingblogPosts))
+
+    res.send(updatedblogPosts)
+})
+
+
+
+
+blogPostsRouter.delete("/:blogPostID", (req, res) => {
+const blogPosts = JSON.parse(fs.readFileSync(blogPostsJSONFilePath))
+
+const remainingblogPosts = blogPosts.filter(a => a.id !== req.params.blogPostID)
+
+fs.writeFileSync(blogPostsJSONFilePath, JSON.stringify(remainingblogPosts))
+
+    res.send('deleted')
+})
+
+export default blogPostsRouter
