a8dea7fdf64de6d893e62e020c65a711c61ebde7
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-09-22 15:09:04 +0100

    Update of Server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,63 @@
-import express from 'express'
-import listEndpoints from 'express-list-endpoints'
-import blogPostsRouter from './services/blogPosts/index.js'
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import blogPostRoute from "./posts/index.js";
+import blogAuthorRoute from "./authors/index.js";
+import filesRouter from "./files/index.js";
+import { join } from "path";
+import listEndpoints from "express-list-endpoints";
 
-const server = express()
+import {
+  badRequestErrorHandler,
+  notFoundErrorHandler,
+  forbiddenErrorHandler,
+  genericServerErrorHandler,
+} from "./errorhandlers.js";
 
-const port = 3001    
+const server = express();
+const port = process.env.PORT || 3001;
+// const port = 3001;
 
-server.use(cors()) 
-server.use(express.json())
+const publicFilePath = join(process.cwd(), "public");
 
-server.use("/blogPosts", blogPostsRouter)
-console.table(listEndpoints(server))
+// Place for our globalmiddlewares
 
+//CORS: Cross-Origin Resource Sharing
+
+const whiteList = [process.env.FRONT_DEV_URL, process.env.FRONT_PROD_URL];
+
+const corsOptions = {
+  origin: function (origin, next) {
+    console.log("CURRENT ORIGIN: ", origin);
+    if (!origin || whiteList.indexOf(origin) !== -1) {
+      next(null, true);
+    } else {
+      next(new Error(`Origin ${origin} not allowed!`));
+    }
+  },
+};
+
+server.use("/files", filesRouter);
+server.use(express.static(publicFilePath));
+server.use(cors(corsOptions));
+server.use(express.json());
+
+// Place for our routes
+server.use("/blogPosts", blogPostRoute);
+server.use("/authors", blogAuthorRoute);
+
+// Place for our error middleware
+
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(genericServerErrorHandler);
+
+console.table(listEndpoints(server));
+
+// Place for console logging port location
 server.listen(port, () => {
-    console.log('server is running now')
-})
+  console.log(`Server running on port ${port}`);
+});
+server.on("error", (error) => {
+  console.log(`Sorry there was an error ${error.message}`);
+});
